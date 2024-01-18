ecbf2431d1749a572db1e982f5cf17027b3c6efb
Daniil Kharitonov <Daniel.Kharitonov@gmail.com>
2024-01-18 23:34:57 +0600

    It was practice on basic syntax and data types

--- GBI_Python_Lec/Lec1.py
+++ GBI_Python_Lec/Lec1.py
@@ -1,1 +1,16 @@
-print(5)
+# a = 5
+# b = 'text'
+# print(a,'-',b)
+# print(f"{a} - {b}")
+# print("{} - {}".format(a,b))
+
+
+# a = int(input ('Введите 1 целое число:'))
+# b = int(input ('Введите 2 целое число:'))
+# print (a, '+', b, '=', b+a )
+# c=a+b
+# print (c)
+
+# a = 8.565
+# b = 9.566
+# print (round(a*b,1))
