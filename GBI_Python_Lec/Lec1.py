5ce8a330ea85ed204a96d01e48b36adec9a70813
Daniil Kharitonov <Daniel.Kharitonov@gmail.com>
2024-01-25 21:56:47 +0600

    It was practice with operators branching and looping and functions with string

--- GBI_Python_Lec/Lec1.py
+++ GBI_Python_Lec/Lec1.py
@@ -14,3 +14,57 @@
 # a = 8.565
 # b = 9.566
 # print (round(a*b,1))
+
+# username = input('Enter your name: ')
+# if username == 'Maria':
+#     print('Yes, you are Maria!')
+# elif username == 'Ilnar':
+#     print('Ilnar you are cool!')
+# else:
+#     print('Hello, ', username)
+
+# i = 0
+# while i<5:
+#     # if i == 3:
+#     #     break
+#     i = i+1
+# else:
+#     print ('Stop:')
+# print (i)
+
+# n = int(input())
+# flag = True
+# i = 2
+# while flag:
+#     if n % i == 0:
+#         flag = False
+#         print (i)
+#     elif i > n // i:
+#         print (n)
+#         flag = False
+#     i += 1
+
+# a = 'string'
+# for i in a:
+#     print(i)
+
+# line = ''
+# for i in range(5):
+#     line = ''
+#     for j in range(5):
+#         line += '+'
+#     print(line)
+
+text = 'съешь еще этих мягких французских булок'
+
+print(text[0])              #c
+# print(text[1])              #ъ
+# print(text[len(text)-1])    #к
+# print(text[-1])             #к
+# print(text[:])              #cъешь еще ... (до конца строки)
+# print(text[:2])             #съ
+# print(text[len(text):2])    #ок
+# print(text[2:9])            #ешь еще
+# print(text[6:-18])          #еще этих мягких
+# print(text[0:len(text):6])  #сеикакл
+# print(text[::6])            #сеикакл
