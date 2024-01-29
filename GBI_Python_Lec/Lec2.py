66de843250445b38fe56dbe1acd33bc39c84dd76
Daniil Kharitonov <Daniel.Kharitonov@gmail.com>
2024-01-29 23:08:09 +0600

    It was practice with lists

---
+++ GBI_Python_Lec/Lec2.py
@@ -0,0 +1,40 @@
+# # Ввод списка:
+# list_1 = []
+# list_1 = list()
+# list_1 = [1,8,3,5,9]
+
+# # Вывод в списка:
+# print (list_1)  #- вывод списка в квадратных скобках
+# print (*list_1) #- вывод списка без квадртных скобок
+
+# for i in list_1:
+#     print(i)
+
+# print(len(list_1))
+# print(list_1[2])
+
+# # Функции для работы со списками:
+# list_1 = [1,5]
+# list_1.append(8) # - .append() добавляет указанный элемент в конце списка
+# print(list_1)
+
+# list_1 = []
+# print(list_1)
+# for i in range(5):
+#     list_1.append(i)
+#     print(list_1 )
+
+# list_1 = [1,12,5,6,-2,5]
+# print(list_1)
+# list_1.pop() # - .pop() удаляет последний элемент из списка
+# print(list_1)
+# list_1.pop(0) # - .pop(n) удаляет n-й элемент из списка
+# print(list_1)
+# print(list_1.pop()) # - .pop() в print - вывод удаляемого элемента
+# print(list_1)
+
+# list_1 = [1,12,5,6,-2,5]
+# print(list_1)
+# list_1.insert(1,333) # - .insert(i,n) - добавляет на i-ю позицию n-й элемент
+# print(list_1)
+
