e325f990fd2937971e5f875a5f01afb87036db08
Daniil Kharitonov <Daniel.Kharitonov@gmail.com>
2024-02-06 21:52:28 +0600

    Into Lesson3/HomeWork/Task3.2 added another solution

--- GBI_Python_Sum/Lesson3/HomeWork/Task3.2.py
+++ GBI_Python_Sum/Lesson3/HomeWork/Task3.2.py
@@ -11,8 +11,19 @@ while volume_numbers<=0:
 import random
 array_numbers = [int(random.random()*10-random.random()*10) for i in range(volume_numbers)]
 print(f'Массив чисел: {array_numbers}')
-search_number = int(input('Введите к какому числу нужно найти ближайшее нименьшее: '))
-array_numbers = [i for i in array_numbers if i < search_number]
-array_numbers.sort()
 
-print(f'Ближайшее наименьшее: {array_numbers.pop()}')
+# search_number = int(input('Введите к какому числу нужно найти ближайшее нименьшее: '))
+# array_numbers = [i for i in array_numbers if i < search_number]
+# array_numbers.sort()
+# print(f'Ближайшее наименьшее: {array_numbers.pop()}')'
+
+# search_number = int(input('Введите к какому числу нужно найти ближайшее: '))
+# right_num = array_numbers[0]
+# for i in array_numbers:
+#     if abs(search_number-i)<abs(search_number-right_num):
+#         right_num = i
+# print(right_num)
+
+search_number = int(input('Введите к какому числу нужно найти ближайшее: '))
+right_num = min(array_numbers, key=lambda x: abs(x - search_number))
+print(right_num)
